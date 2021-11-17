39505ce3f4450a6c4f7b63cede47f0f7bcb145a3
kali-wasim <kali-wasim@kali-wasim.kali>
2021-11-18 00:49:53 +0530

    Well Hello ,This is My First Commit and Push

---
+++ javaPractice/src/javaPractice001/MyArrayList.java
@@ -0,0 +1,15 @@
+package javaPractice001;
+
+public class MyArrayList<E> {
+	Object[] myarr ;
+	int size=0 ;
+	MyArrayList(){
+		myarr = new Object[10];
+	}
+	// adding elements/Objects
+	public void add(E data)
+	{
+		
+	}
+
+}
